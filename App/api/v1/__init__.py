8b562479b80714431b176ebde3ec86c9fc4f6aa9
iot-noob <talhakhalid2018@gmail.com>
2026-03-13 17:03:32 +0500

    blank branch

--- App/api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .UserAuth import router
-from .Admin import admin_router
-from .Users import user_router
-app_router=APIRouter()
-app_router.include_router(router,prefix="/auth")
-app_router.include_router(admin_router,prefix="/admin")
-app_router.include_router(user_router,prefix="/users")
